f1dc46b7a8d0d16c57f9ff741fefe1d4449c53cf
infinityafricatechnology <164397226+infinityafricatechnology@users.noreply.github.com>
2026-08-21 16:16:18 +0000

    Update gsap.ts

--- lib/gsap.ts
+++ lib/gsap.ts
@@ -1,6 +1,7 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+
 // L'enregistrement du plugin ne doit se faire que côté navigateur : ScrollTrigger
 // s'appuie sur des API DOM absentes lors du rendu serveur des composants "use client".
 if (typeof window !== "undefined") {
